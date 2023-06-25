13c998cedaad325ba0062efc1f48c1c38bbc320d
Vaishnavi Anilkumar <vaishnavi20cs@gmail.com>
2023-06-25 09:20:07 +0530

    admin.html
    admin

--- Main/assets/js/admin.js
+++ Main/assets/js/admin.js
@@ -6,6 +6,15 @@ const removeProductBtn = document.querySelector("#remove-product-menu");
 const addContainer = document.querySelector(".add-container");
 const removeContainer = document.querySelector(".remove-container");
 
+// --- Admin Product Management Btns ---
+const additemBtn = document.querySelector("#add-item-menu");
+const removeitemBtn = document.querySelector("#remove-item-menu");
+
+// Admin Management Containers
+const additemContainer = document.querySelector(".add-cont");
+const removeitemContainer = document.querySelector(".remove-cont");
+
+
 // Admin Side URL
 const adminSRC = "http://127.0.0.1:5502/admin-side.html";
 
@@ -19,6 +28,15 @@ if (adminSRC === window.location.href) {
   });
   console.log('JavaScript file loaded.');
 
+  if (adminSRC === window.location.href) {
+    console.log("admin side");
+    // Admin Management Btns
+    additemBtn.addEventListener("click", () => {
+      additemContainer.classList.toggle("show-container");
+      removeitemContainer.classList.remove("show-container");
+    });
+    console.log('JavaScript file loaded.');
+
 // Rest of your code goes here...
 
 
@@ -26,6 +44,11 @@ if (adminSRC === window.location.href) {
   removeProductBtn.addEventListener("click", () => {
     removeContainer.classList.toggle("show-container");
     addContainer.classList.remove("show-container");
+  });
+  // Admin Management Btns
+  removeitemBtn.addEventListener("click", () => {
+    removeitemContainer.classList.toggle("show-container");
+    additemContainer.classList.remove("show-container");
   });
   // Add event listener to "Add to Menu" button
 var addButton = document.querySelector('.add-product .btn');
@@ -69,7 +92,48 @@ removeButton.addEventListener('click', function() {
   // Clear input field
   titleInput.value = '';
 });
+//store
+var addButton = document.querySelector('.add-item .btn');
+addButton.addEventListener('click', function() {
+  var titleInput = document.getElementById('add-title');
+  var priceInput = document.getElementById('add-price');
+  var categorySelect = document.getElementById('add-category');
 
+  // Perform validation on input fields
+  if (titleInput.value === '' || priceInput.value === '') {
+    Swal.fire('Error', 'Please fill in all fields.', 'error');
+    return;
+  }
+
+  // Add the product to the menu (your implementation here)
+
+  // Show success message
+  Swal.fire('Success', 'Product added to menu successfully!', 'success');
+
+  // Clear input fields
+  titleInput.value = '';
+  priceInput.value = '';
+});
+
+// Add event listener to "Remove From Menu" button
+var removeButton = document.querySelector('.remove .btn');
+removeButton.addEventListener('click', function() {
+  var titleInput = document.querySelector('.remove input[type="text"]');
+
+  // Perform validation on input field
+  if (titleInput.value === '') {
+    Swal.fire('Error', 'Please enter the title of the food to remove.', 'error');
+    return;
+  }
+
+  // Remove the product from the menu (your implementation here)
+
+  // Show success message
+  Swal.fire('Success', 'Product removed from menu successfully!', 'success');
+
+  // Clear input field
+  titleInput.value = '';
+});
 
 // Rest of your code goes here...
 
@@ -138,4 +202,5 @@ removeButton.addEventListener('click', function() {
   // Call the function to display all orders
   setOrderDetails();
 
+}
 }
