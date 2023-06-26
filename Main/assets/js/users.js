e707e93f167cae702deca5e6aa8b5f100a136e49
Vishnu Prakash <75669541+Visthebeast@users.noreply.github.com>
2023-06-26 10:13:50 +0530

    remove user button

--- Main/assets/js/users.js
+++ Main/assets/js/users.js
@@ -1,4 +1,3 @@
-
 // Get a reference to the "User_Data" collection
 const usersRef = firebase.database().ref('User_Data');
 
@@ -13,8 +12,39 @@ usersRef.on('value', (snapshot) => {
         const user = childSnapshot.val();
         const li = document.createElement('li');
         li.innerHTML = `<strong>Name:</strong> ${user.User_Name}<br>
-                    <strong>Email:</strong> ${user.Email}<br>
-                    <strong>Phone:</strong> ${user.Phone_Number}<br><br>`;
-        userList.appendChild(li);
+                        <strong>Email:</strong> ${user.Email}<br>
+                        <strong>Phone:</strong> ${user.Phone_Number}<br><br>`;
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove User';
+        removeButton.classList.add('removeButton'); // Add a CSS class for styling
+        removeButton.addEventListener('click', () => {
+            // Remove user from the database
+            childSnapshot.ref.remove()
+                .then(() => {
+                    console.log('User data removed successfully.');
+                })
+                .catch((error) => {
+                    console.log('Error removing user data:', error);
+                });
+
+            // Remove user from the authentication list (assuming you're using Firebase Authentication)
+            const userToDelete = firebase.auth().currentUser;
+            userToDelete.delete()
+                .then(() => {
+                    console.log('User deleted successfully.');
+                })
+                .catch((error) => {
+                    console.log('Error deleting user:', error);
+                });
+        });
+
+        // Create a <div> element to wrap the user details and button
+        const userDetailsContainer = document.createElement('div');
+        userDetailsContainer.classList.add('userDetailsContainer'); // Add a CSS class for styling
+        userDetailsContainer.appendChild(li);
+        userDetailsContainer.appendChild(removeButton);
+
+        userList.appendChild(userDetailsContainer);
     });
 });
